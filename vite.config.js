746acd26dbc3c13c1ff3d69a43fbfee11d524dfa
Erick Islas <erickislasmonrroy23@gmail.com>
2026-03-13 00:14:48 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/stratexpoints/",
   build: {
-    outDir: 'dist',
-    sourcemap: false
+    outDir: "dist"
   }
-})
+});
