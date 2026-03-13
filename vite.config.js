7e63ac490709984aa12890c53691e8cf948fabed
Erick Islas <erickislasmonrroy23@gmail.com>
2026-03-12 23:29:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Cambia 'stratexpoints' por el nombre exacto de tu repositorio en GitHub
+
 export default defineConfig({
   plugins: [react()],
-  base: '/stratexpoints/',
-  build: { outDir: 'dist', sourcemap: false },
-  server: { port: 5173, open: true }
+  base: './',
+  build: { outDir: 'dist' }
 })
