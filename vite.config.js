69f5f1b496d3c115ef78e7964a0a4646abf11a89
Erick Islas <erickislasmonrroy23@gmail.com>
2026-03-12 23:50:11 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: { 
-    outDir: 'dist'
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
